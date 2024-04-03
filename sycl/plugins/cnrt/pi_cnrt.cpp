e4e3199334b92bad7ea4b9120093040cd8b9908b
Ziyang Wang <wangziyang@ict.ac.cn>
2024-04-03 09:39:59 +0000

    update cambricon vendor id

--- sycl/plugins/cnrt/pi_cnrt.cpp
+++ sycl/plugins/cnrt/pi_cnrt.cpp
@@ -1060,7 +1060,7 @@ pi_result cnrt_piDeviceGetInfo(pi_device device, pi_device_info param_name,
                    PI_DEVICE_TYPE_GPU);
   }
   case PI_DEVICE_INFO_VENDOR_ID: {
-    return getInfo(param_value_size, param_value, param_value_size_ret, 4318u);
+    return getInfo(param_value_size, param_value, param_value_size_ret, 51900u);  //cambricon vendor id 0xcabc
   }
   case PI_DEVICE_INFO_MAX_COMPUTE_UNITS: {
     int compute_units = 0;
